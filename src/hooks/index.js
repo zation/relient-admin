4f66c9d8608168fede6a992e1b8f8ea78c2f20d7
Zation <zation1@gmail.com>
2020-04-08 12:31:25 +0800

    add i18n hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,3 +2,4 @@ export useAction from './use-action';
 export useAPITable from './use-api-table';
 export useLocalTable from './use-local-table';
 export useToggleNormalStatus from './use-toggle-normal-status';
+export useI18N from './use-i18n';
---
+++ src/hooks/use-i18n.js
@@ -0,0 +1,4 @@
+import { useContext } from 'react';
+import { I18NContext } from '../contexts';
+
+export default () => useContext(I18NContext);
