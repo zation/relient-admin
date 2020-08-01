2bb7fb00508ce64d890b007a1323642e383f5213
Zation <zation1@gmail.com>
2020-08-02 00:14:33 +0800

    move sum by and field process to relient

--- src/utils/sum-by.js
+++
@@ -1,9 +0,0 @@
-import { flow, prop, reduce, isString } from 'lodash/fp';
-import BigNumber from 'bignumber.js';
-
-export default (handler) => flow(
-  reduce((total, data) => total.plus(
-    isString(handler) ? prop(handler)(data) : handler(data),
-  ), new BigNumber(0)),
-  (total) => total.toNumber(),
-);
