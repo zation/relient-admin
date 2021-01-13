ae05761ec01ff6babe3334e36366e719e3b64221
Liu Yang <yang.liu3@thoughtworks.com>
2021-01-13 10:49:54 +0800

    config filterIcon

--- src/hooks/use-table-search.js
+++ src/hooks/use-table-search.js
@@ -3,46 +3,57 @@ import React, { useMemo, useState } from 'react';
 import { SearchOutlined } from '@ant-design/icons';
 import TableSearch from '../components/table-search';
 
-export default ({ changeFilterValue, dataKey, options, icon, placeholder, width }) => {
+const defaultFilterIcon = <SearchOutlined />;
+
+export default ({
+  changeFilterValue,
+  dataKey,
+  options,
+  filterIcon = defaultFilterIcon,
+  placeholder,
+  width,
+}) => {
   const [filterDropdownVisible, onFilterDropdownVisibleChange] = useState(false);
   const [filteredValue, setFilteredValue] = useState(false);
 
-  return useMemo(() => ({
-    filters: options,
-    filterDropdownVisible,
-    onFilterDropdownVisibleChange,
-    filteredValue, // used for icon highlight
-    filterIcon: <SearchOutlined />,
-    filterDropdown: ({
-      prefixCls,
-      setSelectedKeys,
-      selectedKeys,
-      confirm,
-      clearFilters,
-      filters,
-      visible,
-    }) => (
-      <TableSearch
-        prefixCls={prefixCls}
-        setSelectedKeys={setSelectedKeys}
-        selectedKeys={selectedKeys}
-        clearFilters={clearFilters}
-        filters={filters}
-        visible={visible}
-        confirm={confirm}
-        icon={icon}
-        placeholder={placeholder}
-        width={width}
-        onConfirm={(value) => {
-          changeFilterValue(value, dataKey);
-          setFilteredValue(value ? [value] : undefined);
-          onFilterDropdownVisibleChange(false);
-        }}
-        onReset={() => {
-          setFilteredValue(undefined);
-          changeFilterValue(undefined, dataKey);
-        }}
-      />
-    ),
-  }), [filterDropdownVisible, changeFilterValue, dataKey, icon, placeholder, width]);
+  return useMemo(
+    () => ({
+      filters: options,
+      filterDropdownVisible,
+      onFilterDropdownVisibleChange,
+      filteredValue, // used for icon highlight
+      filterIcon,
+      filterDropdown: ({
+        prefixCls,
+        setSelectedKeys,
+        selectedKeys,
+        confirm,
+        clearFilters,
+        filters,
+        visible,
+      }) => (
+        <TableSearch
+          prefixCls={prefixCls}
+          setSelectedKeys={setSelectedKeys}
+          selectedKeys={selectedKeys}
+          clearFilters={clearFilters}
+          filters={filters}
+          visible={visible}
+          confirm={confirm}
+          placeholder={placeholder}
+          width={width}
+          onConfirm={(value) => {
+            changeFilterValue(value, dataKey);
+            setFilteredValue(value ? [value] : undefined);
+            onFilterDropdownVisibleChange(false);
+          }}
+          onReset={() => {
+            setFilteredValue(undefined);
+            changeFilterValue(undefined, dataKey);
+          }}
+        />
+      ),
+    }),
+    [filterDropdownVisible, changeFilterValue, dataKey, filterIcon, placeholder, width],
+  );
 };
